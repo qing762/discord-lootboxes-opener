a512c517ac9e7148fb4a2378f92d4aba2c1f09ba
qing <threatedblade@outlook.com>
2024-04-07 14:09:17 +0800

    Added new features

--- main.py
+++ main.py
@@ -8,6 +8,39 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def checkDotEnv():
+    checkDotenv = os.path.isfile("./.env")
+    if checkDotenv == True:
+        auth = os.environ.get("AUTH_VALUE")
+        if auth != None:
+            return True
+        else:
+            with open(".env", "w") as x:
+                print("The 'AUTH_VALUE' in the .env file seems to be empty.\n")
+                token = input(
+                    "Enter your Discord user token value, the script will automatically add into your .env file as your 'AUTH_VALUE' value\nValue: "
+                )
+                x.write(f"AUTH_VALUE={token}")
+    else:
+        with open(".env", "w") as x:
+            print(
+                ".env file couldn't be found.\nThis might be your first time running this script. It has been added.\n"
+            )
+            auth = os.environ.get("AUTH_VALUE")
+            if auth != None:
+                print(
+                    "Seems like you had set the 'AUTH_VALUE' in your .env file / pc environment variables. Continuing...\n"
+                )
+                return True
+            else:
+                with open(".env", "w") as x:
+                    print("The 'AUTH_VALUE' in the .env file seems to be empty.\n")
+                    token = input(
+                        "Enter your Discord user token value, the script will automatically add into your .env file as your 'AUTH_VALUE' value\nValue: "
+                    )
+                    x.write(f"AUTH_VALUE={token}")
+
+
 def main():
     auth = os.environ.get("AUTH_VALUE")
 
@@ -34,26 +67,120 @@ def main():
         "Sec-Fetch-Dest": "empty",
     }
     response = requests.post(url, headers=headers)
-    print(response.status_code, response.text)
     if response.status_code == 200:
-        print("Lootbox opened!")
+        item = {
+            "1214340999644446720": "Buster Blade",
+            "1214340999644446721": "Cute Plushie",
+            "1214340999644446722": "Wump Shell",
+            "1214340999644446723": "Speed Boost",
+            "1214340999644446724": "⮕⬆⬇⮕⬆⬇",
+            "1214340999644446725": "Power Helmet",
+            "1214340999644446726": "Quack!!",
+            "1214340999644446727": "OHHHHH BANANA",
+            "1214340999644446728": "Dream Hammer",
+        }
+        if "opened_item" in response.json():
+            openedItem = response.json()["opened_item"]
+            if openedItem in item:
+                print(
+                    f"Lootbox opened! You just got: {item[openedItem]} (ID: {openedItem})"
+                )
+            else:
+                print("The opened item is not recognized.")
     else:
+        print(response.status_code, response.text)
         print("Failed to open lootbox! Status code:", response.status_code)
 
 
 if __name__ == "__main__":
+    checkDotEnv()
     times = input(
         "How many times you wish to open? Leave blank for default (∞)\nValue: "
     )
     print()
+    auth = os.environ.get("AUTH_VALUE")
+    discordID = (
+        str(base64.b64decode(auth.split(".", 1)[0]).decode("utf-8")) if auth else None
+    )
+
+    query = """
+        query Discord($userId: String!) {
+            discord {
+                lookup(userId: $userId) {
+                    user {
+                        id
+                        type
+                        username
+                        displayName
+                        accountAge
+                        createdAt
+                        creationTimestamp
+                        badges {
+                            title
+                            description
+                            url
+                            __typename
+                        }
+                        profileAppearance {
+                            accentColor
+                            avatar {
+                                url
+                                __typename
+                            }
+                            avatarDecoration
+                            banner {
+                                url
+                                __typename
+                            }
+                            __typename
+                        }
+                        __typename
+                    }
+                    __typename
+                }
+                __typename
+            }
+        }
+    """
+
+    response = requests.post(
+        "https://api.discord.name/graphql",
+        headers={
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+            "Content-Length": "749",
+            "Host": "api.discord.name",
+            "Origin": "https://discord.name",
+            "Referer": "https://discord.name/",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-GPC": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "accept": "*/*",
+            "content-type": "application/json",
+            "sec-ch-ua": '"Brave";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "Windows",
+        },
+        json={
+            "operationName": "Discord",
+            "variables": {"userId": discordID},
+            "query": query,
+        },
+    )
+
+    print(
+        f'Login as: {response.json()["data"]["discord"]["lookup"]["user"]["username"]} (ID: {discordID})'
+    )
+    print("\u2500" * 50)
     if times:
         for x in range(int(times)):
             main()
-            print("\n")
             time.sleep(3)
     else:
         while True:
             main()
-            print("\n")
             time.sleep(3)
     print("Done!")
